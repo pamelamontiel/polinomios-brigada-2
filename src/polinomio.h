25f9e87ab89aba25233406db9f060de9280849e5
pamelamontiel <pamelamontielm@gmail.com>
2026-05-09 15:59:37 -0600

    Establish Polinomio structure and function prototypes
    Define the Polinomio data structure and its associated functions for polynomial operations.

--- src/polinomio.h
+++ src/polinomio.h
@@ -1,1 +1,34 @@
-#include "polinomio.h"
+#ifndef __POLINOMIO_H__
+#define __POLINOMIO_H__
+
+#include <stdbool.h>
+#include <stddef.h>
+
+
+// La estructura de datos obligatoria (Punto 5 del PDF)
+typedef struct Termino {
+    float coeficiente;
+    int exponente;
+} Term;
+
+typedef struct Nodo {
+    Term termino;
+    struct Nodo* siguiente;
+    struct Nodo* anterior;
+} Nodo;
+
+typedef struct {
+    Nodo* cabeza;
+    Nodo* cola;
+} Polinomio;
+
+// Requerimientoss funcionales
+Polinomio* crear_polinomio();
+void insertar_termino(Polinomio* p, float coef, int exp); 
+void eliminar_polinomio(Polinomio* p);
+float evaluar_polinomio(Polinomio* p, float x);
+Polinomio* sumar_polinomios(Polinomio* p1, Polinomio* p2);
+Polinomio* multiplicar_polinomios(Polinomio* p1, Polinomio* p2);
+void imprimir_polinomio(Polinomio* p);
+
+#endif
